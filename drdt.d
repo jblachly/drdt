59de2e2970338bc9a3b64f78d4e1f0f071906f74
James Blachly <james.blachly@gmail.com>
2018-03-12 23:19:22 -0400

    Check that -i was supplied

--- drdt.d
+++ drdt.d
@@ -16,8 +16,12 @@ import radiodatafile;
 int main(string[] args) {
 
     string infile;
-    getopt(args, "infile|i", &infile);
     bool is_rdtfile;
+    getopt(args, "infile|i", &infile);
+    if(!infile.length) {
+        writeln("Please specify an input file with -i/--infile");
+        return -1;
+    }
     // TODO check bin/rdt file length is correct
     if (infile[lastIndexOf(infile, '.') .. $] == ".rdt") {
         writeln("RDT file");
@@ -29,36 +33,6 @@ int main(string[] args) {
     auto f = File(infile);
 
     /*
-    auto settings   = new RadioSettings[1];
-
-    auto txtmsgs    = new TextMessage[50];
-    auto contacts   = new ContactInformation[1000];
-    auto rxgroups   = new RxGroup[250];
-    auto zones      = new ZoneInfo[250]; 
-    auto scanlists  = new ScanList[250];
-    auto channels   = new ChannelInformation[1000];
-
-    f.seek(0x2040 + (is_rdtfile ? rdt_headerlen : 0));
-    f.rawRead(settings);
-
-    f.seek(0x2180 + (is_rdtfile ? rdt_headerlen : 0));
-    f.rawRead(txtmsgs);
-
-    f.seek(0x5f80 + (is_rdtfile ? rdt_headerlen : 0));
-    f.rawRead(contacts);
-
-    f.seek(0xEC20 + (is_rdtfile ? rdt_headerlen : 0));
-    f.rawRead(rxgroups);
-
-    f.seek(84997);
-    f.rawRead(zones);
-
-    f.seek(100997);
-    f.rawRead(scanlists);
-
-    f.seek(127013);
-    f.rawRead(channels);
-
     writeln(settings[0].info1.to!string);
     writeln(settings[0].info2.to!string);
     writeln(settings[0].radio_name.to!string);
@@ -66,40 +40,7 @@ int main(string[] args) {
     writeln("Keypad tones    : ", settings[0].keypad_tones);
     writeln("Intro screen: ", settings[0].intro_screen);
     writefln("DMR id: %d", settings[0].radio_dmr_id);
-    //writefln("bit 568:%#x", settings[0].unknown_offset568);
     writefln("Radio programming password: [%s]", settings[0].radio_programming_password);
-
-    foreach( c; contacts) {
-        writeln("Contact name  : ", c.contact_name.to!string);
-        writefln("Contact DMR id: %d", c.contact_dmr_id);
-    }
-
-    foreach( chan; channels ) {
-        writeln( chan );
-        writeln (chan.channel_name.to!string );
-    }
-    
-    foreach( tm; txtmsgs) {
-        writeln( tm);
-        writeln("Empty? ", tm.empty);
-    }
-    */
-
-/*
-    writeln("Now trying OOP:");
-    
-    auto tms =      new Table!(TextMessage)(50, f, is_rdtfile);
-    auto contacts = new Table!(ContactInformation)(1000);
-    auto rxgroups = new Table!(RxGroup)(250);
-    auto zones =    new Table!(ZoneInfo)(250);
-    auto scanlists =new Table!(ScanList)(250);
-    auto channels = new Table!(ChannelInformation)(1000);
-
-    tms.load_from_file(f, is_rdtfile);
-    foreach(tm; tms) {
-        writeln(tm);
-    }
-    writeln("Done!");
     */
 
     writeln("Now with new class interface");
